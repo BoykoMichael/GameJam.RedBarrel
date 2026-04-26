350ac08dd430f694d48461a4b4ca6aee0bdb8514
BoykoMichael <m.d.boiko@student.khai.edu>
2026-04-26 14:21:33 +0300

    write boardui

--- BattleShip/Assets/Scripts/View/BoardUI.cs
+++ BattleShip/Assets/Scripts/View/BoardUI.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class BoardUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Налаштування генерації")]
+    [SerializeField] private GameObject cellPrefab;    // Посилання на префаб клітинки
+    [SerializeField] private Transform gridContainer;  // Посилання на панель PlayerBoardUI
+
+    // Двовимірний масив для швидкого доступу до візуальних клітинок
+    private CellUI[,] visualCells = new CellUI[10, 10];
+
+    private void Start()
     {
-        
+        GenerateBoard();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateBoard()
     {
-        
+        // Проходимо циклами для створення сітки 10х10
+        // Увага: y=0 - це верхній рядок у UI, тому генеруємо зверху вниз
+        for (int y = 0; y < 10; y++)
+        {
+            for (int x = 0; x < 10; x++)
+            {
+                // Створюємо копію префабу і робимо її дочірньою для gridContainer
+                GameObject cellObj = Instantiate(cellPrefab, gridContainer);
+
+                // Називаємо об'єкт для зручності відлагодження
+                cellObj.name = $"Cell_X{x}_Y{y}";
+
+                // Ініціалізуємо скрипт клітинки її координатами
+                CellUI cellUI = cellObj.GetComponent<CellUI>();
+                cellUI.Initialize(x, y);
+
+                visualCells[x, y] = cellUI;
+            }
+        }
+
+        Debug.Log("Візуальне ігрове поле 10х10 успішно згенеровано!");
     }
 }
