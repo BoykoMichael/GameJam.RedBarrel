057a2c8490d3cb0768d82ad4d0c6847eba3af684
Bohdan Ivanchenko <86876483+ferdyt@users.noreply.github.com>
2026-04-26 12:40:31 +0300

    add button

--- BattleShip/Assets/Scripts/MainMenuController.cs
+++ BattleShip/Assets/Scripts/MainMenuController.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Гра закривається...");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Гра закривається...");
     }
 }
